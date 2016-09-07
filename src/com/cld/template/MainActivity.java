f0db70f256dd51ddd92462439f2995e770d84b18
chenpeng <465827509@qq.com>
2016-09-07 14:10:14 +0800

    first

---
+++ src/com/cld/template/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cld.template;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ * 主界面
+ * 
+ * @author ChenP
+ * @date 2016年9月7日 下午12:14:06
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
